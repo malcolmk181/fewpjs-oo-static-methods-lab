eb7925ad99c84ddc967357ec22cd87ee749a3847
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-29 10:48:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return (string.match(/[A-Za-z\-\_\ \']/g) || [""]).join('');
+  }
+
+  static titleize(string) {
+    let words = string.split(' ');
+
+    const ARTICLES = ['the','a','an','but','of','and','for','at','by','from']
+
+    words = words.map((word, index) => {
+      if (index === 0 || !ARTICLES.includes(word)) {
+        // console.log(`word: ${word}`);
+        // console.log(`this.capitalize(word): ${this.capitalize(word)}`);
+        word = this.capitalize(word);
+      }
+      return word;
+    });
+
+    return words.join(' ');
+  }
 }
